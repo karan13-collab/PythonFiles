b6622a5c97d02ff29d785918e79a24f4dbc08ef5
karan13-collab <65488292+karan13-collab@users.noreply.github.com>
2020-05-17 16:23:36 +0530

    Collections.py

---
+++ Collections.py
@@ -0,0 +1,13 @@
+#Assigning Elements To Different Lists
+l1=[1,2,3,5,6,7,"Yes"]
+l2=["Hello",2,[1,2,3],"Hi"]
+print(l1)
+print(l2)
+#Accessing Elements from a Tuple
+t1=(1,2,3)
+print(t1[2])
+print(t1[0])
+#Deleting Different Dictionary Elements
+dic={1:"Nagpur",2:"Mumbai",3:"Delhi"}
+dic.pop(1)
+print(dic)
